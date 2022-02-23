b5ba5bfd1a9e810ec258ebee4281cb12e12a277a
ascinsa <github@ascinsa.net>
2022-02-23 21:59:46 +0000

    inicio de datos Teleservicio

---
+++ odoo_teleservicio/__manifest__.py
@@ -0,0 +1,31 @@
+# _*_ coding: utf-8 _*_
+
+{
+    'name': 'Odoo Teledesvicio',
+    
+    'sumary': ***Teleservicio app to manage Trainig***,
+    
+    'descrption': ***
+        Teleservicio Module to manage Trainig:
+        - Courses
+        - Sessions
+        - Attenddes    
+    ***,
+    
+    'author': 'Yony Luque',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Trainig',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
